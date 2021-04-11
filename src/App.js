8dcffc7a3c52ec2d9bb3a1975f5989ff8492194d
Pedro Guerra <pguerra@cambridgeassociates.com>
2021-04-11 16:04:09 -0300

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 
-import HomePage from './HomePage'
+import HomePage from './pages/homepage/HomePage'
+
+const TopicsList = () => (
+    <div>
+        <h1>TOPICS LIST PAGE</h1>
+    </div>
+)
+
+const TopicDetail = (props) => {
+    console.log(props)
+
+    return (
+        <div>
+            <h1>TOPICS DETAILS PAGE</h1>
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route exact path='/topics' component={TopicsList} />
+                <Route exact path='/topics/:topicId' component={TopicDetail} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,14 @@
+import './HomePage.scss'
+import React from 'react'
+
+import Directory from '../../components/directory/Directory'
+
+function HomePage() {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage
